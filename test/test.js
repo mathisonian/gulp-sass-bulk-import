031ec212f4a26f29a20d5f61f3cb3dbea251561a
Matthew Conlen <mc@mathisonian.com>
2015-03-03 11:43:50 -0500

    add recursive test

--- test/test.js
+++ test/test.js
@@ -23,4 +23,23 @@ describe('gulp-sass-glob-import', function() {
             });
     });
 
+
+    it('should parse a directory recursively', function(done) {
+
+
+        var equalString = '@import "' + __dirname + '/test-scss/recursive-folder/f1.scss' + '";\n';
+        equalString += '@import "' + __dirname + '/test-scss/recursive-folder/f2.scss' + '";\n';
+        equalString += '@import "' + __dirname + '/test-scss/recursive-folder/nested-folder/f3.scss' + '";\n';
+
+        vinyl
+            .src(__dirname + '/test-scss/recursive.scss')
+            .pipe(bulkSass())
+            .on('data', function(file) {
+                expect(file.contents.toString('utf-8').trim()).to.equal(equalString.trim());
+            })
+            .on('end', function() {
+                done();
+            });
+    });
+
 });
