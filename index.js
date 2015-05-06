2648fa0d88c27c302e96b677310265592c179a5c
Denis Borovikov <metaller.ru@gmail.com>
2015-05-06 11:05:51 +0700

    Import only scss/sass files

--- index.js
+++ index.js
@@ -20,7 +20,9 @@ module.exports = function() {
             });
             return replaceString;
         } else {
-            return '@import "' + filename + '";\n'
+            if (filename.substr(-4).match(/sass|scss/i)) {
+                return '@import "' + filename + '";\n'
+            }
         }
     }
 
