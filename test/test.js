495f78e36bdb8d91e5f6061dc7def692d330be66
Matthew Conlen <mc@mathisonian.com>
2015-06-13 15:42:24 -0700

    Add test for single/double quotes re #13

--- test/test.js
+++ test/test.js
@@ -5,10 +5,7 @@ var bulkSass = require('..');
 
 describe('gulp-sass-glob-import', function() {
 
-
     it('should parse a single directory', function(done) {
-
-
         var equalString = '@import "' + __dirname + '/test-scss/import-folder/f1.scss' + '";\n';
         equalString += '@import "' + __dirname + '/test-scss/import-folder/f2.scss' + '";\n';
 
@@ -25,8 +22,6 @@ describe('gulp-sass-glob-import', function() {
 
 
     it('should parse a directory recursively', function(done) {
-
-
         var equalString = '@import "' + __dirname + '/test-scss/recursive-folder/f1.scss' + '";\n';
         equalString += '@import "' + __dirname + '/test-scss/recursive-folder/f2.scss' + '";\n';
         equalString += '@import "' + __dirname + '/test-scss/recursive-folder/nested-folder/f3.scss' + '";\n';
@@ -40,6 +35,27 @@ describe('gulp-sass-glob-import', function() {
             .on('end', function() {
                 done();
             });
+    });
+
+
+    it('should handle single and double quotes', function(done) {
+        var equalString = '@import "' + __dirname + '/test-scss/import-folder/f1.scss' + '";\n';
+        equalString += '@import "' + __dirname + '/test-scss/import-folder/f2.scss' + '";\n';
+        equalString += '@import "' + __dirname + '/test-scss/recursive-folder/nested-folder/f3.scss' + '";\n';
+
+        
+        vinyl
+            .src(__dirname + '/test-scss/single-double-quotes.scss')
+            .pipe(bulkSass())
+            .on('data', function(file) {
+                expect(file.contents.toString('utf-8').trim()).to.equal(equalString.trim());
+            })
+            .on('end', function() {
+                done();
+            });
+
+
+ 
     });
 
 });
